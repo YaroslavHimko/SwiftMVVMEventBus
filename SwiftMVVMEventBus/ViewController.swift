411253cfe8477ed2662d03d9e1ba5c59a63f1d6d
Yaroslav Himko <yaroslav.himko@gmail.com>
2021-05-02 15:23:28 +0300

    Initial commit

---
+++ SwiftMVVMEventBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMVVMEventBus
+//
+//  Created by Yaroslav Himko on 2.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
